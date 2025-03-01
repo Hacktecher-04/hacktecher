38f369eaaa600ff46a8f70b1932045bae97ddab4
Roshanbadgujar <roshanbadgujar87@gmail.com>
2025-03-01 20:15:09 +0530

    f

---
+++ frontend/src/screens/Base.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { gsap } from "gsap";
+
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Health Care</h1>
+            <ul>
+                
+            </ul>
+        </header>
+    );
+}
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2023 Health Care</p>
+        </footer>
+    );
+}
+
+
+export { Header, Footer };
