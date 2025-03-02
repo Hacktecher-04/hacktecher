de5eb0acd93e4d4b2287577c9ec13ca6db8b56cc
Roshanbadgujar <roshanbadgujar87@gmail.com>
2025-03-02 20:39:59 +0530

    g

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -83,7 +83,12 @@ const Home = () => {
                 </section>
 
                 <section className="view-6">
-
+                    <img src="" alt="" />
+                    <div className="content">
+                        <h1>For login</h1>
+                        <p>Trusted care, healthier lives</p>
+                        <button>Login</button>
+                    </div>
                 </section>
             </div>
         </Base>
