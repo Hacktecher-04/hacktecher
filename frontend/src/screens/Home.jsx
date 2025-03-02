e83bedbdecd4d76c6f320cf5cadbf83354e3173b
Roshanbadgujar <roshanbadgujar87@gmail.com>
2025-03-02 18:27:57 +0530

    h

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -7,11 +7,40 @@ const Home = () => {
         <Base>
             <div className="container">
                 <section className="view-1">
-                    <img src="" alt="" />
+                    <img src="src/assets/images/docter.png" alt="" />
                     <div className="text">
                         <h1>Health Care</h1>
                         <p>Trusted care, healthier lives</p>
+                        <button>Get Started</button>
                     </div>
+                </section>
+                <section className="view-2">
+                    <div className="img img-1">
+                        <h1>1</h1>
+                        <p></p>
+                        <button></button>
+                    </div>
+                    <div className="img img-2">
+                        <h1>2</h1>
+                        <p></p>
+                        <button></button>
+                    </div>
+                    <div className="img img-3">
+                        <h1>3</h1>
+                        <p></p>
+                        <button></button>
+                    </div>
+                    <div className="img img-4">
+                        <h1>4</h1>
+                        <p></p>
+                        <button></button>
+                    </div>
+                    <div className="img img-5">
+                        <h1>5</h1>
+                        <p></p>
+                        <button></button>
+                    </div>
+
                 </section>
             </div>
         </Base>
