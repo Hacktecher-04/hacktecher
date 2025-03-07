f9ef3e3c0c02910617adbbc8a016f01a7e65210f
Roshanbadgujar <roshanbadgujar87@gmail.com>
2025-03-07 14:14:20 +0530

    Create About.jsx

---
+++ frontend/src/screens/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Base from "../screens/Base";
+
+const About = () => {
+  return (
+    <Base>
+      <div className="container">
+        <h1>About</h1>
+        <p>
+          This is the about page.   </p></div>
+    </Base>
+  );
+};
+
+export default About;
